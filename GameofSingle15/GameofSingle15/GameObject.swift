ce746ba8523b42cc74bcd3de5291826a45a396c9
Koichi Okada <kokada18@outlook.com>
2016-06-08 16:01:52 -0700

    Integrated main, instruction, option and record view controllers.

---
+++ GameofSingle15/GameofSingle15/GameObject.swift
@@ -0,0 +1,13 @@
+//
+//  GameObject.swift
+//  GameofSingle15
+//
+//  Created by Koichi Okada on 6/7/16.
+//  Copyright © 2016 GregSimons. All rights reserved.
+//
+
+import UIKit
+
+class GameObject: NSObject {
+
+}
